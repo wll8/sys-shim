e745f39cc593e58748152eb76658d6e9943a56dd
wll8 <xwdaysme@gmail.com>
2023-10-07 14:15:31 +0800

    refactor: Remove hwndObj

--- sys.js
+++ sys.js
@@ -14,20 +14,6 @@ console.log(`ws`, process.env.wsUrl)
 const ws = new WebSocket(process.env.wsUrl)
 new Promise(async ()=> {
   const { View, Tray, Msg } = await new Sys(ws)
-  const tray = await new Tray()
-  const msg = await new Msg()
-  await tray.icon(`https://www.hongqiye.com/favicon.ico`)
-  await tray.tip(`tiptiptip`)
-  await tray.pop(`traytray`, `msgmsgmsg`, 1)
-  msg.on(`hello`, (...arg) => {
-    console.log(`hello`, ...arg)
-  })
-  tray.on(`_WM_RBUTTONUP`, () => {
-    console.log(`_WM_RBUTTONUP`, )
-  })
-  tray.on(`_WM_LBUTTONUP`, () => {
-    console.log(`_WM_LBUTTONUP`, )
-  })
   const view = await new View(`${__dirname}/page.html`)
 })
 
