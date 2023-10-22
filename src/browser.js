df4f0216eaff14b47ee597cef45a3504b45dde58
wll8 <xwdaysme@gmail.com>
2023-10-22 16:11:30 +0800

    feat: Allow configuration of parameters such as form and socket

--- src/browser.js
+++ src/browser.js
@@ -1,12 +1,17 @@
 import * as RPCWebSocket from 'rpc-websockets/dist/index.browser-bundle.js'
-import Sys from './sys.js'
-class Sys2 extends Sys {
+import SysRef from './sys.js'
+
+// 当在 webview 中打开时, 有 ext 对象
+global.ext = global.ext || {}
+class Sys extends SysRef {
   constructor(wsUrl = global.ext.wsUrl) {
     return new Promise(async (resolve) => {
       const ws = new RPCWebSocket.Client(await wsUrl)
-      super(ws)
-      resolve(this)
+      // 使用 await 等待 super 返回的 promise 结束, 否则 this 是一个 promise, 不能赋值
+      const that = await super(ws)
+      that.hwnd = await global.ext.hwnd
+      resolve(that)
     })
   }
 }
-global.Sys = Sys2
+global.Sys = Sys
