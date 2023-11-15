db4566440d30598142123d16aaeb53433f3875e0
wll8 <xwdaysme@gmail.com>
2023-11-15 13:03:21 +0800

    feat: token and startup port optimization

--- store/sdk/src/browser.js
+++ store/sdk/src/browser.js
@@ -4,8 +4,9 @@ import SysRef from './sys.js'
 // 当在 webview 中打开时, 有 ext 对象
 global.ext = global.ext || {}
 class Sys extends SysRef {
-  constructor(wsUrl = global.ext.wsUrl) {
+  constructor(wsUrl) {
     return new Promise(async (resolve) => {
+      wsUrl = wsUrl || `${await global.ext.wsUrl}?token=${await global.ext.token }`
       const ws = new RPCWebSocket.Client(await wsUrl)
       // 使用 await 等待 super 返回的 promise 结束, 否则 this 是一个 promise, 不能赋值
       const that = await super(ws)
