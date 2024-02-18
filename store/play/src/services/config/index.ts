3a3aa4128ab6c07d9646ed23167fea515476c5d5
windyeasy <windyeasy13gb@163.com>
2024-02-18 20:30:47 +0800

    chore: discriminate env

---
+++ store/play/src/services/config/index.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const TIME_OUT = 10000
--- store/play/src/services/index.ts
+++ store/play/src/services/index.ts
@@ -1,4 +1,8 @@
+import { BASE_URL, TIME_OUT } from './config'
 import Request from './request'
 
-const request = new Request()
+const request = new Request({
+  baseURL: BASE_URL,
+  timeout: TIME_OUT,
+})
 export default request
