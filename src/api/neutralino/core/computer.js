f3f3f94edae095e3b444b762c488a88da810cfd7
wll8 <xwdaysme@gmail.com>
2024-03-14 20:57:24 +0800

    refactor: Split api into files

---
+++ src/api/neutralino/core/computer.js
@@ -0,0 +1,15 @@
+class Api {
+  constructor(base) {
+    this.base = base
+  }
+  async getOSInfo () {
+    // todo
+    // aar win.versionEx
+    return {
+      "description": `Microsoft Windows 10 专业版`,
+      "name": `Windows NT`,
+      "version": `10.0.19045-4046`,
+    }
+  }
+}
+export default Api
