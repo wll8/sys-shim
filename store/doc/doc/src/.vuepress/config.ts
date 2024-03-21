4f6817575449d2172a3d71cd73ac81b97a3d0d00
wll8 <xwdaysme@gmail.com>
2024-03-21 19:45:35 +0800

    feat: Add window icon

--- store/doc/doc/src/.vuepress/config.ts
+++ store/doc/doc/src/.vuepress/config.ts
@@ -3,30 +3,18 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 import theme from "./theme.js";
 
-const preload = `
-if(globalThis.Sys) {
-  new globalThis.Sys().then(async main => {
-    globalThis.main = main
-    globalThis.native = main.native
-    globalThis.Neutralino = await main.api.neutralino()
-  })
-} else {
-  console.group(
-    "%c提示提示提示:",
-    "background-color: #e0005a ; color: #ffffff ; font-weight: bold ; padding: 4px ;"
-  );
-  console.log( "你可以直接下载 https://github.com/wll8/sys-shim-doc/releases/download/v0.0.1/sys-shim-doc.exe" );
-  console.log( "然后 F12 打开控制台，测试 api 调用" );
-  console.log( "例如 native.process('mspaint')" );
-  console.log( "由于应用未签名，可能报毒，介意误下" );
-  console.groupEnd();
-}
-`
 export default defineUserConfig({  head: [
     [
       "script",
-      {},
-      preload,
+      {src: `/sys-shim-doc/assets/js/page.js`},
+    ],
+    [
+      "link",
+      {
+        rel: `shortcut icon`,
+        href: `/sys-shim-doc/favicon.ico`,
+        type: `image/x-icon`,
+      },
     ],
   ],
   bundler: viteBundler({
---
+++ store/doc/doc/src/.vuepress/public/assets/js/page.js
@@ -0,0 +1,28 @@
+if(globalThis.Sys) {
+  new globalThis.Sys().then(async main => {
+    globalThis.main = main
+    globalThis.native = main.native
+    globalThis.Neutralino = await main.api.neutralino()
+    const [, hwnd] = await main.form.hwnd
+    const title = 'sys-shim 文档'
+    const icon = "https://www.hongqiye.com/favicon.ico"
+    const obj = {hwnd, title, icon}
+    console.log(obj)
+
+    await main.ws.call('run', [`
+      var arg = ...
+      win.form._forms[arg.hwnd].text = arg.title
+      win.form._forms[arg.hwnd].setIcon(arg.icon)
+    `, obj])
+  })
+} else {
+  console.group(
+    "%c提示:",
+    "background-color: #e0005a ; color: #ffffff ; font-weight: bold ; padding: 4px ;"
+  );
+  console.log( "你可以直接下载 https://github.com/wll8/sys-shim-doc/releases/download/v0.0.1/sys-shim-doc.exe" );
+  console.log( "然后 F12 打开控制台，测试 api 调用" );
+  console.log( "例如 native.process('mspaint')" );
+  console.log( "由于应用未签名，可能报毒，介意误下" );
+  console.groupEnd();
+}
