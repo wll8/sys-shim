f4526aef853cb33ea05b840799b8d774b07c5787
wll8 <xwdaysme@gmail.com>
2024-03-27 13:16:47 +0800

    feat: Agents support intercepting properties returned by methods

--- store/sdk/src/main.js
+++ store/sdk/src/main.js
@@ -6,6 +6,7 @@ new globalThis.Sys().then(async main => {
   console.log(`初始化完成`, main)
   globalThis.Neutralino = await main.api.neutralino()
   globalThis.main = main
+  globalThis.native = main.native
   globalThis.msg = new main.Msg()
   createApp(App).mount(`#app`)
 })
