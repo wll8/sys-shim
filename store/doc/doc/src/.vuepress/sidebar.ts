49cbca8de5cc49e77e48b0d816d42bc0845fd9ca
wll8 <xwdaysme@gmail.com>
2024-03-27 12:04:32 +0800

    feat: Fixed directory smoothness

--- store/doc/doc/src/.vuepress/sidebar.ts
+++ store/doc/doc/src/.vuepress/sidebar.ts
@@ -5,27 +5,57 @@ export default sidebar({
     {
       text: "入门指南",
       collapsible: true,
-      prefix: "docs/getting-started",
+      prefix: "docs/getting-started/",
       children: [
         {
           text: `简介`,
-          link: `/docs/getting-started/introduction`,
+          link: `introduction`,
         },
         {
           text: `您的第一个应用`,
-          link: `/docs/getting-started/your-first-app`,
+          link: `your-first-app`,
         },
         {
           text: `使用前端库`,
-          link: `/docs/getting-started/using-frontend-libraries`,
+          link: `using-frontend-libraries`,
         },
       ],
     },
     {
       text: "原生 API",
       collapsible: true,
-      prefix: "docs/api",
-      children: "structure",
+      prefix: "docs/api/",
+      children: [
+        {
+          text: `概览`,
+          collapsible: true,
+          link: "overview",
+        },
+        {
+          text: `fsys`,
+          collapsible: true,
+          prefix: "fsys/",
+          children: `structure`,
+        },
+        {
+          text: `sys`,
+          collapsible: true,
+          prefix: "sys/",
+          children: `structure`,
+        },
+        {
+          text: `win`,
+          collapsible: true,
+          prefix: "win/",
+          children: `structure`,
+        },
+        {
+          text: `inet`,
+          collapsible: true,
+          prefix: "inet/",
+          children: `structure`,
+        },
+      ]
     },
     {
       text: "三方 API",
