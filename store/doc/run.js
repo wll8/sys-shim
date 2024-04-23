e67f594e005fc19e495c294c37a996acff499e96
wll8 <xwdaysme@gmail.com>
2024-04-23 09:49:06 +0800

    style: Adjust code style

--- store/doc/run.js
+++ store/doc/run.js
@@ -1,25 +1,28 @@
+const process = require('node:process')
+
 const sleep = time => new Promise(resolve => setTimeout(resolve, time || 1e3))
-const { ProcessManager } = require(`@wll8/process-manager`);
+const { ProcessManager } = require(`@wll8/process-manager`)
 const cp = new ProcessManager({
   bin: `cmd.exe`,
   arg: [
     `/c`,
     `cd doc && npm run docs:dev`,
   ],
-});
+})
 cp.on(`stdout`, (data = ``) => {
   const [, url] = data.match(/Network:\s+(.*)\n/) || []
   url && runExe(url)
 })
 cp.on(`close`, () => {
-  console.log(`close`);
-});
+  // ...
+})
 
 async function runExe(url) {
-  if(global.runing) {
+  if ( globalThis.runing ) {
     return undefined
   }
-  global.runing = true
+
+  globalThis.runing = true
   const fs = require(`fs`)
   const text = fs.readFileSync(`./package.json`, `utf8`)
   const textJson = JSON.parse(text)
@@ -29,10 +32,10 @@ async function runExe(url) {
   const cp = new ProcessManager({
     bin: `main.exe`,
     autoReStart: false,
-  });
+  })
   cp.on(`close`, () => {
     process.exit()
-  });
+  })
   await sleep()
   fs.writeFileSync(`./package.json`, text)
 }
