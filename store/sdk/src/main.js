1a45b391570ad965d901cfe01d790dcd0f72fa09
wll8 <xwdaysme@gmail.com>
2024-04-25 18:10:42 +0800

    把 run 方法改为多线程（大体积参数无效）

--- store/sdk/src/main.js
+++ store/sdk/src/main.js
@@ -4,7 +4,7 @@ import './browser.js'
 
 new globalThis.Sys({log: true}).then(async main => {
   console.log(`初始化完成`, main)
-  globalThis.Neutralino = await main.api.neutralino()
+  // globalThis.Neutralino = await main.api.neutralino()
   globalThis.main = main
   globalThis.native = main.native
   globalThis.msg = new main.Msg()
