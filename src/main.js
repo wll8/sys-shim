8893d2cf76d6d9dee1e79a56aff563fa6774bf99
wll8 <xwdaysme@gmail.com>
2024-05-22 11:43:01 +0800

    fix: There should be no errors when passing in multiple lines of code
    ``` text
    sys.native.process.popen.cmd(`
      cd /d C:\\
      dir
      echo %time%
    `).readAll()
    ```

--- src/main.js
+++ src/main.js
@@ -7,13 +7,25 @@ import App from './App.vue'
 // 从发布的包里引入
 import Sys from 'sys-shim'
 
-new Sys({log: true}).then(async main => {
-  console.log(`初始化完成`, main)
-  // globalThis.Neutralino = await main.api.neutralino()
-  globalThis.main = main
-  globalThis.native = main.native
-  globalThis.msg = new main.Msg()
+const fn = async sys => {
+  console.log(`初始化完成`, sys)
+  globalThis.sys = sys
+  globalThis.main = sys
+  globalThis.ws = sys.ws
+  globalThis.native = sys.native
+  globalThis.msg = new sys.Msg()
+}
+
+new Promise(async () => {
+  if(globalThis.sys) {
+    await fn(globalThis.sys)
+  } else {
+    await new Sys({
+      log: true,
+      wsUrl: `ws://127.0.0.1:10005?token=tokentokentoken`,
+    }).then(fn).catch(err => {
+      console.error(`sys-shim 初始化失败`)
+    })
+  }
   createApp(App).mount(`#app`)
-}).catch(err => {
-  console.error(`初始化错误`, err)
 })
