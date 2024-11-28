85c38d41a110659984ae1dda53f8b62fd48614ff
wll8 <xwdaysme@gmail.com>
2024-11-28 16:14:04 +0800

    feat: 在 preload 中演示设置窗口图标

--- store/sdk/template/pack/preload.js
+++ store/sdk/template/pack/preload.js
@@ -1,5 +1,9 @@
 document.addEventListener(`DOMContentLoaded`, async () => {
   new window.Sys().then(async main => {
     window.sys = main
+    // 编程式设置窗口图标
+    const tabIcon = `./favicon.ico`
+    const [, exist] = await window.sys.native.io.exist(tabIcon)
+    exist && await window.sys.native.win.image.setIcon(window.sys.hwnd, tabIcon)
   })
 })
