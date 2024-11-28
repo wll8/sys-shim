2d1fd1f090d90a83875ba4226012295098a5e245
wll8 <xwdaysme@gmail.com>
2024-11-28 16:12:40 +0800

    feat: 为 cli 添加 logo

--- store/sdk/script/npm-pkg/bin/index.mjs
+++ store/sdk/script/npm-pkg/bin/index.mjs
@@ -12,12 +12,15 @@ import { getPath } from 'sys-shim-bin'
 const binPath = getPath()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const pkgPath = `${__dirname}/../../../`
 
-const cwd = `${__dirname}/../shim/win`
+const cwd = `${pkgPath}/script/npm-pkg/shim/win`
+const pkg = JSON.parse(fs.readFileSync(`${pkgPath}/package.json`, `utf8`))
 const argv = minimist(process.argv.slice(2))
 const argv0 = argv._[0]
 
 new Promise(async () => {
+  printLogo()
   if(argv0 === `pack`) {
     await pack(argv)
   } else {
@@ -27,3 +30,18 @@ new Promise(async () => {
     })
   }
 })
+
+
+function printLogo() {
+  const vTag = `version: `
+  const logText = fs.readFileSync(`${__dirname}/logo.txt`, `utf8`)
+  const versionLogo = logText.replace(new RegExp(`(${vTag})(.*)`), (match, $1, $2) => {
+    const vStr = pkg.version
+    const vLength = vStr.length
+    const vLine = vLength > $2.length // 如果版本号替换到版本标志后面
+      ? `${$1}${vStr}`
+      : match.replace(new RegExp(`(${vTag})(.{${vLength}})`), `$1${vStr}`)
+    return vLine
+  })
+  console.log(versionLogo)
+}
