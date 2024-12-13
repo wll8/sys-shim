a17bbc1fc01fcd896a04b3e4e987e0e48ebf5a1f
wll8 <xwdaysme@gmail.com>
2024-12-13 17:59:12 +0800

    refactor: 移除冗余代码

--- store/demo/live-control-robot/src/config.js
+++ store/demo/live-control-robot/src/config.js
@@ -1,15 +1,3 @@
-import DisableDevtool from 'disable-devtool'
-if (process.env.NODE_ENV !== `development`) {
-  // DisableDevtool({
-  //   timeOutUrl: `https://example.com/`,
-  //   disableMenu: false,
-  //   ondevtoolopen(type, next) {
-  //     console.log(`Devtool opened with type:` + type)
-  //     next()
-  //   },
-  // })
-}
-
 export default {
   development: {
     baseApi: `http://127.0.0.1:7800/`,
