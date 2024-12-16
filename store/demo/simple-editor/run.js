cb1fd4f48d9f0c4f2bd32a32ac79b2973f84e12b
wll8 <xwdaysme@gmail.com>
2024-12-16 09:46:35 +0800

    chore: 更新依赖

--- store/demo/simple-editor/run.js
+++ store/demo/simple-editor/run.js
@@ -1,42 +1,61 @@
 import process from 'node:process'
+import child_process from 'node:child_process'
 import fs from 'node:fs'
+import { getPath } from 'sys-shim-bin'
+const sysShimBinPath = getPath()
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time || 1e3))
 
-const sleep = time => new Promise(resolve => setTimeout(resolve, time || 1e3))
 new Promise(async () => {
+  const VITE_SERVER_BASEURL = `ws://127.0.0.1:10005?token=tokentokentoken`
   const { ProcessManager } = await import(`@wll8/process-manager`)
-  console.log(`ProcessManager`, ProcessManager)
-  const cp = new ProcessManager({
+  const treeKill = (await import(`tree-kill`)).default
+  const cpVite = new ProcessManager({
     bin: `cmd.exe`,
-    arg: [
-      `/c`,
-      `npx vite`,
-    ],
+    arg: [`/k`, `npx vite`],
+    spawnOption: {
+      env: {
+        VITE_SERVER_BASEURL,
+      },
+    },
   })
-  cp.on(`stdout`, (data = ``) => {
+  cpVite.on(`stdout`, (data = ``) => {
     const [, url] = data.match(/Network:\s+(.*)\n/) || []
     url && runExe(url)
   })
-  cp.on(`close`, () => {
-    // ...
+  cpVite.on(`close`, () => {
+    process.exit()
   })
 
   async function runExe(url) {
-    if ( globalThis.runing ) {
+    url = `${url}/init.html`
+    if (globalThis.runing) {
       return undefined
     }
 
     globalThis.runing = true
+
     const text = fs.readFileSync(`./package.json`, `utf8`)
-    const textJson = JSON.parse(text)
-    textJson.page = url
+    let textJson = JSON.parse(text)
+    textJson = {
+      ...textJson,
+      page: url,
+      token: `tokentokentoken`,
+      browserArguments: `--disable-web-security --allow-running-insecure-content`,
+      originRegExp: `.*`,
+      form: {
+        right: `1000`,
+        bottom: `600`,
+      },
+      socket: {
+        port: 10005,
+      },
+    }
     fs.writeFileSync(`./package.json`, JSON.stringify(textJson, null, 2))
     await sleep()
-    const cp = new ProcessManager({
-      bin: `node_modules/sys-shim/script/npm-pkg/shim/win/main.exe`,
-      autoReStart: false,
-    })
-    cp.on(`close`, () => {
-      process.exit()
+    child_process.exec(sysShimBinPath, (...arg) => {
+      treeKill(process.pid, `SIGKILL`, (err) => {
+        process.exit()
+      })
     })
     await sleep()
     fs.writeFileSync(`./package.json`, text)
