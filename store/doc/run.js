b760249b57b148d437041c7883df9b3ca3b16bda
wll8 <xwdaysme@gmail.com>
2024-12-16 10:20:10 +0800

    chore: 不使用工作区的形式
    - 因为兼容性不好

--- store/doc/run.js
+++ store/doc/run.js
@@ -1,4 +1,9 @@
+const fs = require('node:fs')
 const process = require('node:process')
+const treeKill = require('tree-kill')
+const child_process = require('node:child_process')
+const { getPath } = require('sys-shim-bin')
+const sysShimBinPath = getPath()
 
 const sleep = time => new Promise(resolve => setTimeout(resolve, time || 1e3))
 const { ProcessManager } = require(`@wll8/process-manager`)
@@ -18,23 +23,34 @@ cp.on(`close`, () => {
 })
 
 async function runExe(url) {
-  if ( globalThis.runing ) {
+  if (globalThis.runing) {
     return undefined
   }
 
   globalThis.runing = true
-  const fs = require(`fs`)
+
   const text = fs.readFileSync(`./package.json`, `utf8`)
-  const textJson = JSON.parse(text)
-  textJson.page = url
+  let textJson = JSON.parse(text)
+  textJson = {
+    ...textJson,
+    page: url,
+    token: `tokentokentoken`,
+    browserArguments: `--disable-web-security --allow-running-insecure-content`,
+    originRegExp: `.*`,
+    form: {
+      right: `1000`,
+      bottom: `600`,
+    },
+    socket: {
+      port: 10005,
+    },
+  }
   fs.writeFileSync(`./package.json`, JSON.stringify(textJson, null, 2))
   await sleep()
-  const cp = new ProcessManager({
-    bin: `main.exe`,
-    autoReStart: false,
-  })
-  cp.on(`close`, () => {
-    process.exit()
+  child_process.exec(sysShimBinPath, (...arg) => {
+    treeKill(process.pid, `SIGKILL`, (err) => {
+      process.exit()
+    })
   })
   await sleep()
   fs.writeFileSync(`./package.json`, text)
--- store/play/run.cjs
+++ store/play/run.cjs
@@ -1,5 +1,10 @@
+const fs = require('node:fs')
 const process = require('node:process')
+const child_process = require('node:child_process')
+const treeKill = require('tree-kill')
+const { getPath } = require('sys-shim-bin')
 
+const sysShimBinPath = getPath()
 const sleep = time => new Promise(resolve => setTimeout(resolve, time || 1e3))
 const { ProcessManager } = require(`@wll8/process-manager`)
 const cp = new ProcessManager({
@@ -10,6 +15,7 @@ const cp = new ProcessManager({
   ],
 })
 cp.on(`stdout`, (data = ``) => {
+  // eslint-disable-next-line regexp/no-super-linear-backtracking
   const [, url] = data.match(/Network:\s+(.*)\n/) || []
   url && runExe(url)
 })
@@ -18,22 +24,34 @@ cp.on(`close`, () => {
 })
 
 async function runExe(url) {
-  if (globalThis.runing)
+  if (globalThis.runing) {
     return undefined
+  }
 
   globalThis.runing = true
-  const fs = require(`fs`)
+
   const text = fs.readFileSync(`./package.json`, `utf8`)
-  const textJson = JSON.parse(text)
-  textJson.page = url
+  let textJson = JSON.parse(text)
+  textJson = {
+    ...textJson,
+    page: url,
+    token: `tokentokentoken`,
+    browserArguments: `--disable-web-security --allow-running-insecure-content`,
+    originRegExp: `.*`,
+    form: {
+      right: `1000`,
+      bottom: `600`,
+    },
+    socket: {
+      port: 10005,
+    },
+  }
   fs.writeFileSync(`./package.json`, JSON.stringify(textJson, null, 2))
   await sleep()
-  const cp = new ProcessManager({
-    bin: `main.exe`,
-    autoReStart: false,
-  })
-  cp.on(`close`, () => {
-    process.exit()
+  child_process.exec(sysShimBinPath, () => {
+    treeKill(process.pid, `SIGKILL`, () => {
+      process.exit()
+    })
   })
   await sleep()
   fs.writeFileSync(`./package.json`, text)
